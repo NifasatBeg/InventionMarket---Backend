f7efcf560f373b3a4460bf1b992b20f069cd2fa6
Personal <personal@Nifasats-MacBook-Pro.local>
2024-06-27 10:21:34 +0530

    Project setup

---
+++ src/main/java/com/personal/InventionMarket/InventionMarketApplication.java
@@ -0,0 +1,13 @@
+package com.personal.InventionMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventionMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventionMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/InventionMarket/InventionMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.InventionMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventionMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
